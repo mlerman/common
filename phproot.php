56821d67b492defc8f11cf46664c2958cd540d37
mlerman <mlerman@users.noreply.github.com>
2025-04-10 09:03:48 -0700

    update of phproot and index in common

--- phproot.php
+++ phproot.php
@@ -2,5 +2,7 @@
 echo "this is phproot.php in the common directory<br/>";
 
 echo "<a href='downloadfile.php?fname=ui_test_associate_extension.run&targetdir=C:\\UniServer\\www\doc\\files\\ThisPC\\install_wampserver\\install_php_standalone&admin=0'>test associate RUN extension</a><br/>";
-
 ?>
+
+<hr/>
+<a href="index.php">index.php</a><br/>
